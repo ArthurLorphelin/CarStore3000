d0e3a04d05b6c529435834facc139753f81e3992
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2021-09-23 14:43:52 +0200

    Test unit file test.py

---
+++ app/tests/test.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+
+class Test(unittest.TestCase):
+
+	def set_up(self):
+		self.app = launch_app()
+		self.buyer = self.test_client()
+
+	def test_homePage(self):
+		response = self.buyer().get('/')
+		self.assertEqual(response.data, b'Test complete! Welcome!')
+
+if __name__ = '__main__':
+	unittest.main()
